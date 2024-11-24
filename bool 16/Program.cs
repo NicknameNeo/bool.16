581d80df038cae79af7414fc8d481c40df06b781
My Name <abdulloevaslan96@gmail.com>
2024-11-24 08:50:52 +0300

    MyProject

---
+++ bool 16/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace bool_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var result = (a >= 10 && a <= 99);
+            bool result1 = (a % 2 == 0);
+            
+            
+            Console.WriteLine(result1);
+
+        }
+    }
+}
+
